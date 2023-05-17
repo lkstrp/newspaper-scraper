53f43be2f0f74c21c13d620c855b725ca8512d45
lkstrp <lkstrp@pm.me>
2023-05-16 21:37:46 -0500

    Added log level setting

--- newspaper_scraper/settings.py
+++ newspaper_scraper/settings.py
@@ -10,15 +10,28 @@ class _Settings:
     """
 
     _log_file = 'logs.log'
+    _log_level = 'DEBUG'
     retry_on_exception = True
     save_interval = 60
     selenium_driver = None
 
     _description = {
         'log_file': 'Path to the log file. If None, no log file is created.',
+        'log_level': 'Log level. Possible values: DEBUG, INFO, WARNING, ERROR, CRITICAL.',
         'retry_on_exception': 'If True, retry on exception.',
         'save_interval': 'Interval in seconds to save the database. Smaller values will slow down the process.',
+        'selenium_driver': 'Selenium driver object. Pass a driver object to use it if the provided one does not work.'
     }
+
+    # noinspection PyMissingOrEmptyDocstring
+    @property
+    def log_level(self):
+        return self._log_level
+
+    @log_level.setter
+    def log_level(self, value):
+        self._log_level = value
+        logger.change_log_level(logger.log, value)
 
     # noinspection PyMissingOrEmptyDocstring
     @property
--- newspaper_scraper/utils/logger.py
+++ newspaper_scraper/utils/logger.py
@@ -42,3 +42,32 @@ def change_log_file_path(logger: logging.Logger, new_log_file: str):
         new_file_handler.setLevel(logging.DEBUG)
         new_file_handler.setFormatter(fmt)
         logger.addHandler(new_file_handler)
+
+def change_log_level(logger: logging.Logger, new_log_level):
+    """
+    Changes the log level of the given logger. If the logger does not have a level set, the new level will be applied.
+    The new_log_level can be provided as an integer or a string representing the log level name.
+
+    Args:
+        logger (logging.Logger): Logger to change the log level of.
+        new_log_level: New log level (int or str).
+    """
+    level_name_to_level = {
+        'CRITICAL': logging.CRITICAL,
+        'ERROR': logging.ERROR,
+        'WARNING': logging.WARNING,
+        'INFO': logging.INFO,
+        'DEBUG': logging.DEBUG,
+        'NOTSET': logging.NOTSET
+    }
+
+    if isinstance(new_log_level, str):
+        new_log_level = new_log_level.upper()
+        if new_log_level not in level_name_to_level:
+            raise ValueError(f"Invalid log level name '{new_log_level}'.")
+        new_log_level = level_name_to_level[new_log_level]
+
+    if isinstance(new_log_level, int) and (0 <= new_log_level <= 50):
+        logger.setLevel(new_log_level)
+    else:
+        raise ValueError("Invalid log level. Log level must be an integer between 0 and 50 or a valid log level name.")
